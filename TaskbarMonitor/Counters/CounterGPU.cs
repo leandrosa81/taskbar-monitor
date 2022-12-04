7da6e1ddedca34da77bc50b3434ac96daef46917
ouned <michael@losert.org>
2022-12-04 19:32:06 +0100

    get new instance of PerformanceCounter each time it seems to have a limited lifetime

--- TaskbarMonitor/Counters/CounterGPU.cs
+++ TaskbarMonitor/Counters/CounterGPU.cs
@@ -3,43 +3,48 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TaskbarMonitor.Counters
 {
     class CounterGPU : ICounter
     {
-        List<PerformanceCounter> gpuCounters;
-
         public CounterGPU(Options options)
             : base(options)
-        {
-
-        }
+        {}
 
         public override void Initialize()
         {
-            var category = new PerformanceCounterCategory("GPU Engine");
-            var counterNames = category.GetInstanceNames();
-
-            gpuCounters = counterNames
-                                .Where(counterName => counterName.EndsWith("engtype_3D"))
-                                .SelectMany(counterName => category.GetCounters(counterName))
-                                .Where(counter => counter.CounterName.Equals("Utilization Percentage"))
-                                .ToList();
-
             lock (ThreadLock)
             {
                 InfoSummary = new CounterInfo() { Name = "GPU Summary", History = new List<float>(), MaximumValue = 100.0f };
                 Infos = new List<CounterInfo>();
                 Infos.Add(new CounterInfo() { Name = "GPUM", History = new List<float>(), MaximumValue = 100.0f });
             }
-
-            gpuCounters.ForEach(x => x.NextValue());
         }
         public override void Update()
         {
-            var currentValue = gpuCounters.Sum(x => x.NextValue());
+            float currentValue = 0;
+
+            try
+            {
+                var category = new PerformanceCounterCategory("GPU Engine");
+                var counterNames = category.GetInstanceNames();
+
+                List<PerformanceCounter> gpuCounters =gpuCounters = counterNames
+                                        .Where(counterName => counterName.EndsWith("engtype_3D"))
+                                        .SelectMany(counterName => category.GetCounters(counterName))
+                                        .Where(counter => counter.CounterName.Equals("Utilization Percentage"))
+                                        .ToList();
+
+                gpuCounters.ForEach(x => x.NextValue());
+                currentValue = gpuCounters.Sum(x => x.NextValue());
+            }
+            catch (InvalidOperationException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
 
             lock (ThreadLock)
             {
